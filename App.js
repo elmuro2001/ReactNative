c0f22a26eef9760a72f3974bb7edb2546c5ea362
Aitor Muro Perez <aitormuroperez@gmail.com>
2025-05-06 09:46:34 +0200

    Initial commit
    Generated by create-expo-app 3.4.2.

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+<<<<<<< HEAD
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Campobase from './componentes/CampobaseComponent';
@@ -13,12 +14,28 @@ export default function App() {
       <StatusBar style="auto" />
     </View>
   </SafeAreaProvider>
+=======
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+>>>>>>> 70bce62 (Initial commit)
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+<<<<<<< HEAD
 
+=======
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+>>>>>>> 70bce62 (Initial commit)
   },
 });
